db846c01c1f4fcd405f49c2c7e69876997ad9f8e
gus-bms <jgk03077@naver.com>
2023-02-16 20:22:45 +0900

    반응형 작업 (헤더)

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,9 +1,13 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { faBars, faMinus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import style from '../styles/Header.module.scss'
 
 export default function Header() {
   const pbRef = useRef<HTMLDivElement | null>(null);
-
+  const [isMobile, setIsMobile] = useState<boolean>(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+  const [isToggle, setIsToggle] = useState<boolean>(false)
+  const menuRef = useRef<HTMLDivElement | null>(null)
   const handleLogo = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' })
   }
@@ -18,6 +22,10 @@ export default function Header() {
     }
   }
 
+  const resizeListener = (e: any) => {
+    if (e.target.visualViewport.width > 961) setIsToggle(false)
+  };
+
   useEffect(() => {
     window.onscroll = function () {
       var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -26,20 +34,44 @@ export default function Header() {
       if (pbRef.current)
         pbRef.current.style.width = scrolled + "%";
     }
+
+    window.addEventListener("resize", resizeListener);
+
   }, [])
+
+  useEffect(() => {
+    console.log(isToggle)
+    if (isToggle) {
+      menuRef.current?.classList.add(style.active)
+      return
+    }
+    menuRef.current?.classList.remove(style.active)
+
+  }, [isToggle])
 
   return (
     <>
       <div className={style.box__header}>
         <h1 onClick={handleLogo} className={style.logo}>Gus-Bms</h1>
-        <div className={style.box__menu} style={{ marginLeft: 'auto', display: 'flex' }}>
-          <h2 onClick={() => moveAnchor('history')}>history</h2>
-          <h2 onClick={() => moveAnchor('projects')} >projects</h2>
-          <h2 >certi</h2>
+        <div className={style.box__menu} >
+          <FontAwesomeIcon icon={!isToggle ? faBars : faMinus} color='black' size='2x' onClick={() => setIsToggle(!isToggle)} />
+          <div ref={menuRef} >
+            <h2 onClick={() => {
+              setIsToggle(false)
+              moveAnchor('history')
+            }
+            }>history</h2>
+            <h2 onClick={() => {
+              setIsToggle(false)
+              moveAnchor('projects')
+            }
+            }>projects</h2>
+            <h2 >certi</h2>
+          </div>
         </div>
-      </div>
-      <div className={style.progress__container}>
-        <div ref={pbRef} className={style.progress__bar}></div>
+        <div className={style.progress__container}>
+          <div ref={pbRef} className={style.progress__bar}></div>
+        </div>
       </div>
     </>
   )
