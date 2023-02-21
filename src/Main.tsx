21371259bd0ac39c8c63cb9dd5f81be03de47315
gus-bms <jgk03077@naver.com>
2023-02-21 20:30:50 +0900

    프로젝트 사진 업데이트

--- src/Main.tsx
+++ src/Main.tsx
@@ -12,9 +12,10 @@ export default function Main() {
   const nightRef = useRef<SVGSVGElement | null>(null)
   const [isDaymode, setIsDayMode] = useState<boolean>(true) //true : day false: night
 
-  const findPhotoImgs = ['find_photo1', 'find_photo2', 'find_photo3', 'find_photo4']
-  const portFolioImgs = ['portfolio_1', 'portfolio_2', 'portfolio_1']
+  const findPhotoImgs = ['find_photo1', 'find_photo2', 'find_photo3', 'find_photo4', 'find_photo5', 'find_photo6', 'find_photo7']
+  const portFolioImgs = ['portfolio_1', 'portfolio_2', 'portfolio_3']
   const gludocMobileImgs = ['gludoc_mobile_1', 'gludoc_mobile_2', 'gludoc_mobile_3', 'gludoc_mobile_4', 'gludoc_mobile_5', 'gludoc_mobile_6', 'gludoc_mobile_7']
+  const ilabImgs = ['ilab_2', 'ilab_3', 'ilab_4', 'ilab_5', 'ilab_6', 'ilab_7', 'ilab_8']
 
   useEffect(() => {
     if (dayRef.current && nightRef.current) {
@@ -289,7 +290,7 @@ export default function Main() {
           </div>
           <div className={style.project__detail}>
             <div className={style.box__slider}>
-              <Slider images={findPhotoImgs} />
+              <Slider images={ilabImgs} />
             </div>
             <div className={style.box__intro}>
               <div className={style.intro}>
